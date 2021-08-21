e654e3057981feb71122a97f3b833d3bc9824d75
nurcanaksu <nurcan.aksuf@gmail.com>
2021-08-21 13:43:08 +0300

    Update App.js

--- hrms-react/src/App.js
+++ hrms-react/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import "semantic-ui-css/semantic.min.css";
+
 import { Container } from "semantic-ui-react";
 import Navi from "./layouts/NaviLayout/Navi";
 import Dashboard from "./layouts/DashboardLayout/Dashboard";
@@ -12,7 +13,7 @@ function App() {
       <Container className="main">
         <Dashboard />
       </Container>
-      <Footer />
+      <Footer/>
     </div>
   );
 }
